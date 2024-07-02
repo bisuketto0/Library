a2cf095f5f2fe4ee29ca45c59cd13d490749f246
dani <123590342+bisuketto0@users.noreply.github.com>
2024-07-02 16:11:56 -0300

    Main #8 - Make book card more responsive - Make cancel button on modal work

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const openModal = document.querySelector('.add-book');
+const closeModal = document.querySelector('#close-modal')
 const modal = document.querySelector('.form-modal');
 
 openModal.addEventListener('click', () => {
   modal.showModal();
+})
+
+closeModal.addEventListener('click', () => {
+  modal.close();
 })
