813ba8eceadd9763db29242cdf65ad770b1e8f0c
dani <123590342+bisuketto0@users.noreply.github.com>
2024-07-17 13:23:23 -0300

    Submit book funciton #2 - add book to DOM

--- app.js
+++ app.js
@@ -33,6 +33,52 @@ function addBookToLibrary(book) {
 defaultBook = Book('Galatians', 'Paul', 5, true, bookID);
 addBookToLibrary(defaultBook);
 
+function addBookCard(title, author, pages, haveRead, id) {
+  const nodeBookCard = document.createElement('div');
+  const nodeBookInfo = document.createElement('div');
+  const nodeBookTitle = document.createElement('h3');
+  const nodeBookAuthor = document.createElement('span');
+  const nodeBookPages = document.createElement('span');
+  const nodeBookAction = document.createElement('div');
+  const nodeBookHaveRead = document.createElement('button');
+  const nodeBookRemove = document.createElement('button');
+
+  nodeBookCard.classList.add('book-card');
+  nodeBookInfo.classList.add('book-info');
+  nodeBookTitle.classList.add('book-title');
+  nodeBookAuthor.classList.add('book-author');
+  nodeBookPages.classList.add('book-pages');
+  nodeBookAction.classList.add('book-action');
+  nodeBookHaveRead.classList.add('toggle-read');
+  nodeBookRemove.classList.add('remove-book');
+
+  nodeBookTitle.textContent = title;
+  nodeBookAuthor.textContent = author;
+  nodeBookPages.textContent = pages;
+  if(haveRead) {
+    nodeBookHaveRead.classList.add('read');
+    nodeBookHaveRead.textContent = 'Read'
+  } else {
+    nodeBookHaveRead.classList.add('not-read');
+    nodeBookHaveRead.textContent = 'Not Read'
+  }
+  nodeBookRemove.textContent = 'Remove';
+  nodeBookCard.dataset.id = id;
+
+  appendBookCard(nodeBookCard, nodeBookInfo, nodeBookTitle, nodeBookAuthor, nodeBookPages, nodeBookAction, nodeBookHaveRead, nodeBookRemove);
+}
+
+function appendBookCard(nodeBook, nodeInfo, nodeTitle, nodeAuhor, nodePages, nodeAction, nodeRead, nodeRemove ) {
+  document.querySelector('.card-ctn').appendChild(nodeBook);
+  nodeBook.appendChild(nodeInfo);
+  nodeBook.appendChild(nodeAction);
+  nodeInfo.appendChild(nodeTitle);
+  nodeInfo.appendChild(nodeAuhor);
+  nodeInfo.appendChild(nodePages);
+  nodeAction.appendChild(nodeRead);
+  nodeAction.appendChild(nodeRemove);
+}
+
 function submitBook() {
   let book;
   bookID++;
@@ -42,6 +88,7 @@ function submitBook() {
   bookHaveRead = document.querySelector('.book-form').elements[3].checked;
   book = new Book(bookTitle, bookAuthor, bookPages, bookHaveRead, bookID);
   addBookToLibrary(book);
+  addBookCard(bookTitle, bookAuthor, bookPages, bookHaveRead, bookID);
 }
 
 submitBookButton.addEventListener('click', submitBook);
