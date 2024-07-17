1156ebf7ef40665ac962613e22b6efb72f06dbd3
dani <123590342+bisuketto0@users.noreply.github.com>
2024-07-17 12:18:48 -0300

    Submit book function #1 - Add submitBook function to the submit book   button

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-const openModal = document.querySelector('.add-book');
-const closeModal = document.querySelector('#close-modal')
+const openModalButton = document.querySelector('.add-book');
+const closeModalButton = document.querySelector('#close-modal')
 const modal = document.querySelector('.form-modal');
+const submitBookButton = document.querySelector('#submit-book');
+let bookTitle;
+let bookAuthor;
+let bookPages;
+let bookHaveRead;
+let bookID = 0;
 
-openModal.addEventListener('click', () => {
+openModalButton.addEventListener('click', () => {
   modal.showModal();
 })
 
-closeModal.addEventListener('click', () => {
+closeModalButton.addEventListener('click', () => {
   modal.close();
 })
+
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
+  this.id = id;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function submitBook() {
+  let book;
+  bookTitle = document.querySelector('.book-form').elements[0].value;
+  bookAuthor = document.querySelector('.book-form').elements[1].value;
+  bookPages = document.querySelector('.book-form').elements[2].value;
+  bookHaveRead = document.querySelector('.book-form').elements[3].checked;
+  book = new Book(bookTitle, bookAuthor, bookPages, bookHaveRead, bookID);
+  addBookToLibrary(book);
+  bookID++;
+}
+
+submitBookButton.addEventListener('click', submitBook);
